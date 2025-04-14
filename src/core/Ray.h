6204e7204ba73aa9ecb92c57133db1eb3f465872
Chukwudi M Ikem <chukwudimikem@gmail.com>
2025-04-13 18:47:17 -0700

    vec.h ray.h mods

--- src/core/Ray.h
+++ src/core/Ray.h
@@ -7,20 +7,17 @@ class Object;
 class Ray
 {
 public:
-    vec3 endpoint; // endpoint of the ray where t=0
+    vec3 origin; // origin of the ray where t=0
     vec3 direction; // direction the ray sweeps out - unit vector
 
     Ray()
-        :endpoint(0,0,0),direction(0,0,1)
+        :origin(0,0,0),direction(0,0,1)
     {}
 
-    Ray(const vec3& endpoint_input,const vec3& direction_input)
-        :endpoint(endpoint_input),direction(direction_input.normalized())
+    Ray(const vec3& origin_input,const vec3& direction_input)
+        :origin(origin_input),direction(direction_input.normalized())
     {}
 
-    vec3 Point(double t) const
-    {
-        return endpoint+direction*t;
-    }
+    vec3 Point(double t) const { return origin + direction * t;}
 };
 #endif
--- src/core/Vec.h
+++ src/core/Vec.h
@@ -1,5 +1,5 @@
-#ifndef __vec__
-#define __vec__
+#ifndef __VEC__
+#define __VEC__
 
 #include <cmath>
 #include <iostream>
@@ -14,63 +14,63 @@ template<class T, int n>
 struct vec {
     T x[n];
 
-    inline vec() { make_zero(); }
+    vec() { make_zero(); }
 
-    inline explicit vec(const T& a) {
+    explicit vec(const T& a) {
         assert(n == 1); x[0] = a;
     }
 
-    inline vec(const T& a, const T& b) {
+    vec(const T& a, const T& b) {
         assert(n == 2); x[0] = a; x[1] = b;
     }
 
-    inline vec(const T& a, const T& b, const T& c) {
+    vec(const T& a, const T& b, const T& c) {
         assert(n == 3); x[0] = a; x[1] = b; x[2] = c;
     }
 
     template<class U>
-    inline explicit vec(const vec<U,n>& v) {
+    explicit vec(const vec<U,n>& v) {
         for(int i = 0; i < n; i++) x[i] = (T)v.x[i];
     }
 
-    inline void make_zero() { fill(0); }
-    inline void fill(T value) { for(int i = 0; i < n; i++) x[i] = value; }
+    void make_zero() { fill(0); }
+    void fill(T value) { for(int i = 0; i < n; i++) x[i] = value; }
 
-    inline vec& operator += (const vec& v) { for(int i = 0; i < n; i++) x[i] += v.x[i]; return *this; }
-    inline vec& operator -= (const vec& v) { for(int i = 0; i < n; i++) x[i] -= v.x[i]; return *this; }
-    inline vec& operator *= (const vec& v) { for(int i = 0; i < n; i++) x[i] *= v.x[i]; return *this; }
-    inline vec& operator /= (const vec& v) { for(int i = 0; i < n; i++) x[i] /= v.x[i]; return *this; }
-    inline vec& operator *= (const T& c) { for(int i = 0; i < n; i++) x[i] *= c; return *this; }
-    inline vec& operator /= (const T& c) { for(int i = 0; i < n; i++) x[i] /= c; return *this; }
+    vec& operator += (const vec& v) { for(int i = 0; i < n; i++) x[i] += v.x[i]; return *this; }
+    vec& operator -= (const vec& v) { for(int i = 0; i < n; i++) x[i] -= v.x[i]; return *this; }
+    vec& operator *= (const vec& v) { for(int i = 0; i < n; i++) x[i] *= v.x[i]; return *this; }
+    vec& operator /= (const vec& v) { for(int i = 0; i < n; i++) x[i] /= v.x[i]; return *this; }
+    vec& operator *= (const T& c) { for(int i = 0; i < n; i++) x[i] *= c; return *this; }
+    vec& operator /= (const T& c) { for(int i = 0; i < n; i++) x[i] /= c; return *this; }
 
-    inline vec operator + () const { return *this; }
-    inline vec operator - () const { vec r; for(int i = 0; i < n; i++) r[i] = -x[i]; return r; }
+    vec operator + () const { return *this; }
+    vec operator - () const { vec r; for(int i = 0; i < n; i++) r[i] = -x[i]; return r; }
 
-    inline vec operator + (const vec& v) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] + v.x[i]; return r; }
-    inline vec operator - (const vec& v) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] - v.x[i]; return r; }
-    inline vec operator * (const vec& v) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] * v.x[i]; return r; }
-    inline vec operator / (const vec& v) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] / v.x[i]; return r; }
+    vec operator + (const vec& v) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] + v.x[i]; return r; }
+    vec operator - (const vec& v) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] - v.x[i]; return r; }
+    vec operator * (const vec& v) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] * v.x[i]; return r; }
+    vec operator / (const vec& v) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] / v.x[i]; return r; }
 
-    inline vec operator * (const T& c) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] * c; return r; }
-    inline vec operator / (const T& c) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] / c; return r; }
+    vec operator * (const T& c) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] * c; return r; }
+    vec operator / (const T& c) const { vec r; for(int i = 0; i < n; i++) r[i] = x[i] / c; return r; }
 
-    inline const T& operator[] (int i) const { return x[i]; }
-    inline T& operator[] (int i) { return x[i]; }
+    const T& operator[] (int i) const { return x[i]; }
+    T& operator[] (int i) { return x[i]; }
 
-    inline T magnitude_squared() const { return dot(*this, *this); }
-    inline T magnitude() const { return sqrt(magnitude_squared()); }
+    T magnitude_squared() const { return dot(*this, *this); }
+    T magnitude() const { return sqrt(magnitude_squared()); }
 
-    inline vec normalized() const {
+    vec normalized() const {
         T mag = magnitude();
         return mag ? *this / mag : vec();
     }
 };
 
 template <class T, int n>
-inline vec<T,n> operator * (const T& c, const vec<T,n>& v) { return v * c; }
+vec<T,n> operator * (const T& c, const vec<T,n>& v) { return v * c; }
 
 template <class T, int n>
-inline T dot(const vec<T,n> & u, const vec<T,n> & v)
+T dot(const vec<T,n> & u, const vec<T,n> & v)
 {
     T r  =  0;
     for(int i = 0; i < n; i++) r += u.x[i] * v.x[i];
@@ -78,7 +78,7 @@ inline T dot(const vec<T,n> & u, const vec<T,n> & v)
 }
 
 template <class T >
-inline vec<T,3> cross(const vec<T,3> & u, const vec<T,3> & v)
+vec<T,3> cross(const vec<T,3> & u, const vec<T,3> & v)
 {
     return vec<T,3> (
         u[1] * v[2] - u[2] * v[1],
@@ -87,7 +87,7 @@ inline vec<T,3> cross(const vec<T,3> & u, const vec<T,3> & v)
 }
 
 template<class T, int d>
-inline vec<T,d> componentwise_max(const vec<T,d>& a, const vec<T,d>& b)
+vec<T,d> componentwise_max(const vec<T,d>& a, const vec<T,d>& b)
 {
     vec<T,d> r;
     for(int i=0; i<d; i++) r[i] = std::max(a[i], b[i]);
@@ -95,7 +95,7 @@ inline vec<T,d> componentwise_max(const vec<T,d>& a, const vec<T,d>& b)
 }
 
 template<class T, int d>
-inline vec<T,d> componentwise_min(const vec<T,d>& a, const vec<T,d>& b)
+vec<T,d> componentwise_min(const vec<T,d>& a, const vec<T,d>& b)
 {
     vec<T,d> r;
     for(int i=0; i<d; i++) r[i] = std::min(a[i], b[i]);
@@ -103,7 +103,7 @@ inline vec<T,d> componentwise_min(const vec<T,d>& a, const vec<T,d>& b)
 }
 
 template <class T, int n>
-inline std::ostream& operator << (std::ostream& out, const vec<T,n> & u)
+std::ostream& operator << (std::ostream& out, const vec<T,n> & u)
 {
     for(int i = 0; i < n; i++)
     {
@@ -114,7 +114,7 @@ inline std::ostream& operator << (std::ostream& out, const vec<T,n> & u)
 }
 
 template <class T, int n>
-inline std::istream& operator >> (std::istream& in, vec<T,n> & u)
+std::istream& operator >> (std::istream& in, vec<T,n> & u)
 {
     for(int i = 0; i < n; i++)
     {
@@ -123,11 +123,11 @@ inline std::istream& operator >> (std::istream& in, vec<T,n> & u)
     return in;
 }
 
-typedef vec<double,2> vec2;
-typedef vec<double,3> vec3;
-typedef vec<int,2> ivec2;
-typedef vec<int,3> ivec3;
-typedef vec<float,2> vec2f;
-typedef vec<float,3> vec3f;
+typedef vec<double,2> vec2;   // 2D double vector
+typedef vec<double,3> vec3;   // 3D double vector
+typedef vec<int,2> ivec2;     // 2D int vector
+typedef vec<int,3> ivec3;     // 3D int vector
+typedef vec<float,2> vec2f;   // 2D float vector
+typedef vec<float,3> vec3f;   // 3D float vector
 
 #endif
