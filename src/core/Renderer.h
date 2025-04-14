d1060c1c7da586c3ba4345ae68b2c1b108e0bfcf
Chukwudi M Ikem <chukwudimikem@gmail.com>
2025-04-13 18:20:14 -0700

    created Renderer.h, added headed code, also added header code to Scene.h

---
+++ src/core/Renderer.h
@@ -0,0 +1,22 @@
+#ifndef __RENDERER_H__
+#define __RENDERER_H__
+
+#include "core/Vec.h"
+#include "core/Ray.h"
+#include "utils/ImageWriter.h"
+#include "core/Scene.h"
+
+// Renderer class contians the logic for rendering the scene. The Renderer are Scene class are tightly coupled.
+class Renderer {
+public:
+    int image_width, image_height, spp; // samples per pixel, default is 1 for now
+
+    Renderer(int w, int h, int samples = 1)
+        : image_width(w), image_height(h), spp(samples) {}
+    // Pass world data to this renderer.
+    void Render(Scene& scene);
+    void Render_Pixel(Scene& scene, const ivec2& pixel);
+    void Initialize_Hierarchy(Scene& scene);
+};
+
+#endif
---
+++ src/core/Scene.h
@@ -0,0 +1,60 @@
+#ifndef __SCENE_H__
+#define __SCENE_H__
+
+#include <vector>
+#include <memory>
+#include "core/Ray.h"
+#include "core/Vec.h"
+#include "geometry/Object.h"
+#include "lights/Light.h"
+#include "core/Camera.h"
+#include "geometry/BVH.h"
+
+// TODO: forward declations, as classs are not defined yet! REMOVE THIS LINE LATER!
+class Object;
+class Light;
+class Camera;
+class BVH;
+class Hit;
+// TODO: THIS LINE ^ REMOVE IT LATER!
+
+// Scene holds all the objects, lights, and camera in the scene. (essentially the world data)
+class Scene {
+public:
+    // using smart pointers for memory management, for the sake of safety and convenience, RAII!
+    std::unique_ptr<Camera> camera;                     // camera for the scene
+    std::vector<std::shared_ptr<Object>> objects;       // scene geometry
+    std::vector<std::shared_ptr<Light>> lights;         // emissive sources
+    std::shared_ptr<BVH> bvh;                           // acceleration structure
+
+    vec3 ambient_color;
+    double ambient_intensity;
+
+    bool enable_shadows;
+    int recursion_depth_limit;
+
+    Scene();
+    Scene(const Scene&) = delete; // disable copy constructor, unique pointers cannot be copied, strict ownership
+    Scene& operator=(const Scene&) = delete; // disable copy assignment, unique pointers cannot be copied
+    Scene(Scene&&) = default; // enable move semantics
+    Scene& operator=(Scene&&) = default; // enable move assignment
+    ~Scene() = default; // no need for custom destructor given smart pointers
+    
+    // essentially the same as const Object* obj, but obj knows when to delete itself 
+    void AddObject(const std::shared_ptr<Object>& obj) {
+        objects.push_back(obj);
+    }
+
+    void AddLight(const std::shared_ptr<Light>& light) {
+        lights.push_back(light);
+    }
+
+    void BuildBVH() {
+        bvh = std::make_shared<BVH>(objects);
+    }
+
+    vec3 CastRay(const Ray& ray, int depth) const;
+    Hit ClosestIntersection(const Ray& ray) const;
+};
+
+#endif
