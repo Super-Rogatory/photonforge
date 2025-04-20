1beee06f3b5d2fff6ebcfa4462ad482bd0854155
Chukwudi M Ikem <chukwudimikem@gmail.com>
2025-04-19 21:35:31 -0700

    Removed Object and Hit forward declarations from Scene.h

---
+++ src/utils/Constants.h
@@ -0,0 +1,5 @@
+// SOME DESIGN NOTES:
+// 1.) If the object is not a mesh, the part will be -1.
+// 2.) Small_t is defined in the Constants.h file and will be imported inside Vec.h.
+// 3.) Attributes of a class are in snake_case, while methods are in camelCase.
+static const double small_t = 1e-4;
