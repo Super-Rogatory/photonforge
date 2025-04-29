027c329d4eaef5d65f276cb81e69cd2798692566
Chukwudi M Ikem <chukwudimikem@gmail.com>
2025-04-29 10:38:13 -0700

    bvh revised - shows full image, fixed few bugs

--- src/geometry/BVH.h
+++ src/geometry/BVH.h
@@ -1,40 +1,38 @@
 #ifndef __BVH_H__
 #define __BVH_H__
 
-#include "../core/Vec.h"
-#include "../geometry/AABB.h"
-#include "../geometry/Object.h"
-#include "../geometry/Hit.h"
-#include "../core/Ray.h"
+
 #include <vector>
-#include <algorithm>
-
-struct BVHNode {
-    AABB boundingBox;
-    int objectStartIndex;
-    int objectCount;
-    int leftChildIndex;   // NEW
-    int rightChildIndex;  // existing
-
-    bool isLeaf() const { return leftChildIndex == -1 && rightChildIndex == -1; } // updated leaf check
-};
-
+#include <memory>
+#include "core/Ray.h"
+#include "geometry/Object.h"
+#include "geometry/AABB.h"
+#include "geometry/Hit.h"
 
 class BVH {
-    public:
-        BVH(const std::vector<std::shared_ptr<Object>>& objects);
+public:
+    BVH(const std::vector<std::shared_ptr<Object>>& objects);
+    Hit intersect(const Ray& ray) const;
 
-        Hit intersect(const Ray& ray) const;
-        AABB getBoundingBox() const;
 
-    private:
-        static const int MAX_NUM_OBJECTS = 4; // 4 is a common max num
-        std::vector<BVHNode> nodes;
-        std::vector<std::shared_ptr<Object>> primitives; // just the objects, usually referred to as primitives
+private:
+    struct BVHNode {
+        AABB bbox;
+        std::shared_ptr<BVHNode> left;
+        std::shared_ptr<BVHNode> right;
+        std::vector<std::shared_ptr<Object>> primitives;
 
-        //recursive
-        void BuildBVH(int nodeIndex, int start, int end); 
-        void IntersectNode(int nodeIndex, const Ray& ray, Hit& closestHit, double t_min, double& t_max) const; 
+
+        bool isLeaf() const {
+            return !primitives.empty();
+        }
+    };
+
+    std::shared_ptr<BVHNode> root;
+
+    std::shared_ptr<BVHNode> build(std::vector<std::shared_ptr<Object>>& objects, int depth = 0);
+    Hit traverse(const BVHNode* node, const Ray& ray) const;
 };
+
 
 #endif
